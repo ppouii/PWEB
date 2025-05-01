4d86076b00b01ed5c8e1042ebc35fb74366eaaf2
Deivid Alessandro Dias Gomes <141849461+ppouii@users.noreply.github.com>
2025-04-30 22:07:08 -0300

    Add files via upload

---
+++ Atividade13/Atividade13.js
@@ -0,0 +1,22 @@
+function JanelaAberta(){
+    let img = document.getElementById("Janela")
+    img.setAttribute('src','JanelaAberta.jpg')
+    document.getElementById("texto").innerHTML="Janela aberta"
+   
+    
+}
+function JanelaFechada(){
+    let img = document.getElementById("Janela")
+    img.setAttribute('src','JanelaFechada.png')
+    document.getElementById("texto").innerHTML="Janela Fechada"
+    
+    
+}
+
+function JanelaQuebrada(){
+    let img = document.getElementById("Janela")
+    img.setAttribute('src','JanelaQuebrada.jpg')
+    document.getElementById("texto").innerHTML="Janela Quebrada"
+    
+    
+}
