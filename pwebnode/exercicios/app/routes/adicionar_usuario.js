8e83ce205da2e8beaf2ef6b4f7c8cf184b00e485
Deivid Alessandro Dias Gomes <141849461+ppouii@users.noreply.github.com>
2025-06-04 21:33:11 -0300

    Add files via upload

---
+++ pwebnode/exercicios/app/routes/adicionar_usuario.js
@@ -0,0 +1,4 @@
+module.exports= function(app){
+app.get('/admin/adicionar_usuario', function(req,res){
+    res.render("admin/adicionar_usuario");
+})}
---
+++ pwebnode/exercicios/app/routes/professores.js
@@ -0,0 +1,34 @@
+module.exports = function(app){
+    app.get('/informacao/professores', function(req,res){
+            //res.render("informacao/professores");
+        const sql = require('mssql');
+const { options } = require('../config/server');
+
+const sqlConfig = {
+    user: 'BD2313018',
+    password: 'Ppouii1546',
+    database:'BD',
+    server:'APOLO',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+    }
+}
+async function getProfessores() {
+    try{
+        const pool = await sql.connect(sqlConfig);
+
+        const result =  await pool.request().query('select*from professores')
+
+        res.json(result.recordset);
+
+        res.render("informacao/professores",{profs: result.recordset})
+
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+getProfessores();
+});
+}
